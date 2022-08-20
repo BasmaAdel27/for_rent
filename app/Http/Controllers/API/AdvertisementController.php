72e7349b63ae13fe0ac4055ad4d3d90bf2f57c9d
basma adel <basmaadelmohamed@outlook.com>
2022-08-21 01:09:44 +0200

    image in update

--- app/Http/Controllers/API/AdvertisementController.php
+++ app/Http/Controllers/API/AdvertisementController.php
@@ -353,8 +353,7 @@ class AdvertisementController extends Controller
         "type" => $request->type,
         "area"=> $request->area,
         "address" => $request->address,
-
-        "control" => "pending",
+        "control" =>"pending",
         "price"=>$request->price,
         "city_id" =>$request->city_id
     ]);
